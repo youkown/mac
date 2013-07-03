b284853ee5ea37d33d0fe8a79cf1783a70a17361
xuexiongwei <xuexiongwei@e750b0c1-14f6-384e-aab6-32adc2919cd5>
2013-07-03 02:58:34 +0000

    git-svn-id: https://sinonet:8443/svn/mobilesales/codes/server/trunks/SinoNetFramework@139 e750b0c1-14f6-384e-aab6-32adc2919cd5

--- src/main/java/net/lc4ever/framework/context/ContextLoaderListener.java
+++ src/main/java/net/lc4ever/framework/context/ContextLoaderListener.java
@@ -5,7 +5,6 @@
 package net.lc4ever.framework.context;
 
 import javax.servlet.ServletContextEvent;
-import javax.servlet.annotation.WebListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +14,6 @@ import org.slf4j.LoggerFactory;
 /**
  * @author <a href="mailto:apeidou@gmail.com">Q-Wang</a>
  */
-@WebListener
 public class ContextLoaderListener extends org.springframework.web.context.ContextLoaderListener {
 
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
